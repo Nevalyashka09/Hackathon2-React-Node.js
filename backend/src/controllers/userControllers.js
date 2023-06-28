349d23b8229979191065d3983964e0dac56e75d5
OLESIA TUR <olesia.tur@gmail.com>
2023-06-28 19:20:39 +0200

    backend roots

---
+++ backend/src/controllers/userControllers.js
@@ -0,0 +1,87 @@
+const models = require("../models");
+
+const getAllUsers = (req, res) => {
+  models.user
+    .findAll()
+    .then(([user]) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUserById = (req, res) => {
+  models.user
+    .find(req.params.id)
+    .then(([user]) => {
+      if (user[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(user[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const createUser = (req, res) => {
+  const user = req.body;
+
+  models.user
+    .addUser(user)
+    .then(([result]) => {
+      res.location(`/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const updateUser = (req, res) => {
+  const user = req.body;
+
+  user.user_id = parseInt(req.params.id, 10);
+
+  models.user
+    .updateUser(user)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.user
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  destroy,
+};
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+const userControllers = require("./controllers/userControllers");
+
+router.get("/users", userControllers.getAllUsers);
+router.get("/users/:id", userControllers.getUserById);
+
 module.exports = router;
