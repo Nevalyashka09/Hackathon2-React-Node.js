fdd085840202c6a5927f3c6f0709d41373df8ce7
Henry DEMAISON <henry31150@hotmail.fr>
2023-06-28 16:46:36 +0200

    template done

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -2,10 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Root() {
   return (
-    <>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <main>
+      <Outlet />
+    </main>
   );
 }
