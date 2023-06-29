48a8d5b156c817434a16c15be6d24186f163d030
Henry DEMAISON <henry31150@hotmail.fr>
2023-06-29 14:36:22 +0200

    couloeur et font ajoutés

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,40 @@ module.exports = {
     preflight: false,
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
+      transitionDuration: {
+        2000: "2000ms",
+      },
+      dropShadow: {
+        top: "0px -8px 16px rgba(0, 0, 0, 0.10)",
+      },
+      colors: {
+        primary: {
+          50: "#CF2E79",
+          70: "#439BCD",
+          100: "#A5C444",
+          800: "#FAF4FB",
+          900: "#EBEBEB",
+        },
+        secondary: {
+          600: "#439BCD",
+        },
+        chip: {
+          blue: "#2196F3",
+          orange: "#ED6C02",
+          green: "#AFE2B1",
+        },
+        status: {
+          green: "#AFE2B1",
+        },
+        user: {
+          warningRed: "#D32F2F",
+        },
+      },
+    },
   },
   plugins: [],
 };
