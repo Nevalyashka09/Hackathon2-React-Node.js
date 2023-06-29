77bed981465149adf18927d16a618c47b777ff55
Eva Demmer <eva.demmer@gmail.com>
2023-06-29 12:19:42 +0200

    Login done.

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const userControllers = require("./controllers/userControllers");
 const smartphoneControllers = require("./controllers/smartphoneControllers");
+const verifyPassword = require("./middlewares/verifyPassword");
 
 router.get("/users", userControllers.getAllUsers);
 router.get("/users/:id", userControllers.getUserById);
+router.post("login/", verifyPassword);
 
 router.get("/smartphones", smartphoneControllers.getAllSmartphones);
 router.get("/smartphones/brands", smartphoneControllers.getSmartphonesbyBrand);
